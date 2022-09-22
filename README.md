7bd4e9777f6ca156bfc2ee12d3581a455d42beb1
RubixDev <silas.groh@t-online.de>
2022-09-22 17:44:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# mason-update-all
+Easily update all [Mason](https://github.com/williamboman/mason.nvim) packages with one command.
+
+* [Requirements](#requirements)
+* [Installation](#installation)
+  * [Packer](#packer)
+  * [vim-plug](#vim-plug)
+* [Setup](#setup)
+* [Commands](#commands)
+* [Events](#events)
+
+## Requirements
+- [`mason.nvim`](https://github.com/williamboman/mason.nvim)
+
+## Installation
+### [Packer](https://github.com/wbthomason/packer.nvim)
+```lua
+use { 'RubixDev/mason-update-all' }
+```
+
+### [vim-plug](https://github.com/junegunn/vim-plug)
+```vim
+Plug 'RubixDev/mason-update-all'
+```
+
+## Setup
+```lua
+require('mason-update-all').setup()
+```
+
+## Commands
+- `:MasonUpdateAll` — update all installed Mason packages
+
+## Events
+Upon completion of all updates the user event `MasonUpdateAllComplete` will be emitted. You can use it like so:
+
+```lua
+vim.api.nvim_create_autocmd('User', {
+    pattern = 'MasonUpdateAllComplete',
+    callback = function()
+        print('mason-update-all has finished')
+    end,
+})
+```
+
+or in VimScript:
+
+```vim
+autocmd User MasonUpdateAllComplete echo 'mason-update-all has finished'
+```
---
+++ lua/mason-update-all/init.lua
@@ -0,0 +1,56 @@
+local registry = require('mason-registry')
+
+local M = {}
+
+function M.update_all()
+    local any_update = false -- Whether any package was updated
+    local running_count = 0 -- Currently running jobs
+
+    print('[mason-update-all] Fetching updates')
+    -- Iterate installed packages
+    for _, pkg in ipairs(registry.get_installed_packages()) do
+        running_count = running_count + 1
+
+        -- Fetch for new version
+        pkg:check_new_version(function(new_available, version)
+            if new_available then
+                any_update = true
+                print(
+                    ('[mason-update-all] Updating %s from %s to %s'):format(
+                        pkg.name,
+                        version.current_version,
+                        version.latest_version
+                    )
+                )
+                pkg:install():on('closed', function()
+                    running_count = running_count - 1
+                    print(('[mason-update-all] Updated %s to %s'):format(pkg.name, version.latest_version))
+                end)
+            else
+                running_count = running_count - 1
+            end
+
+            -- Done
+            if running_count == 0 then
+                if any_update then
+                    print('[mason-update-all] Finished updating all packages')
+                else
+                    print('[mason-update-all] Nothing to update')
+                end
+
+                -- Trigger autocmd
+                vim.schedule(function()
+                    vim.api.nvim_exec_autocmds('User', {
+                        pattern = 'MasonUpdateAllComplete',
+                    })
+                end)
+            end
+        end)
+    end
+end
+
+function M.setup()
+    vim.api.nvim_create_user_command('MasonUpdateAll', M.update_all, {})
+end
+
+return M
