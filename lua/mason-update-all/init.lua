68d9d893b039a870c3400cde17fb0485f9934c3e
RubixDev <silas.groh@t-online.de>
2023-07-10 12:03:45 +0800

    fix: update registry before checking versions

--- lua/mason-update-all/init.lua
+++ lua/mason-update-all/init.lua
@@ -24,36 +24,51 @@ function M.update_all()
     local running_count = 0 -- Currently running jobs
 
     print('[mason-update-all] Fetching updates')
-    -- Iterate installed packages
-    for _, pkg in ipairs(registry.get_installed_packages()) do
-        running_count = running_count + 1
+    -- Update the registry
+    registry.update(function(success, err)
+        if not success then
+            print('[mason-update-all] Error fetching updates: ' .. err)
 
-        -- Fetch for new version
-        pkg:check_new_version(function(new_available, version)
-            if new_available then
-                any_update = true
-                print(
-                    ('[mason-update-all] Updating %s from %s to %s'):format(
-                        pkg.name,
-                        version.current_version,
-                        version.latest_version
+            -- Trigger autocmd
+            vim.schedule(function()
+                vim.api.nvim_exec_autocmds('User', {
+                    pattern = 'MasonUpdateAllComplete',
+                })
+            end)
+            return
+        end
+
+        -- Iterate installed packages
+        for _, pkg in ipairs(registry.get_installed_packages()) do
+            running_count = running_count + 1
+
+            -- Fetch for new version
+            pkg:check_new_version(function(new_available, version)
+                if new_available then
+                    any_update = true
+                    print(
+                        ('[mason-update-all] Updating %s from %s to %s'):format(
+                            pkg.name,
+                            version.current_version,
+                            version.latest_version
+                        )
                     )
-                )
-                pkg:install():on('closed', function()
+                    pkg:install():on('closed', function()
+                        running_count = running_count - 1
+                        print(('[mason-update-all] Updated %s to %s'):format(pkg.name, version.latest_version))
+
+                        -- Done
+                        check_done(running_count, any_update)
+                    end)
+                else
                     running_count = running_count - 1
-                    print(('[mason-update-all] Updated %s to %s'):format(pkg.name, version.latest_version))
+                end
 
-                    -- Done
-                    check_done(running_count, any_update)
-                end)
-            else
-                running_count = running_count - 1
-            end
-
-            -- Done
-            check_done(running_count, any_update)
-        end)
-    end
+                -- Done
+                check_done(running_count, any_update)
+            end)
+        end
+    end)
 end
 
 function M.setup()
